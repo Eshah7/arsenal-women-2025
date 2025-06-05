ff0dd7239d3fc6d4168197811115ac159f93d879
Esha Shah <67078410+Eshah7@users.noreply.github.com>
2025-06-05 18:27:09 -0400

    Updating Theme

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,11 @@
 import streamlit as st
 
+st.set_page_config(
+    page_title="Arsenal Women 2024-2025 Player Analysis",
+    page_icon="📊",
+    layout="wide"
+)
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
